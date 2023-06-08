89a7bf80dd7ae65dcbaea9d5c16811544e3d5f66
Naman1997 <aroranaman17@gmail.com>
2023-06-08 23:04:46 +0530

    Updated main-1.tf

--- main-1.tf
+++ main-1.tf
@@ -44,20 +44,3 @@ module "Vpc" {
   }
 }
 
-module "Subnet" {
-  cidr_block              = "192.168.128.0/19"
-  map_public_ip_on_launch = false
-  az                      = "use1-az1"
-  tags                    = {
-    aws:cloudformation:logical-id = "SubnetPrivateUSEAST1B"
-    kubernetes.io/role/internal-elb = "1"
-    aws:cloudformation:stack-name = "eksctl-kaiburrEks-cluster"
-    Name = "eksctl-kaiburrEks-cluster/SubnetPrivateUSEAST1B"
-    alpha.eksctl.io/cluster-name = "kaiburrEks"
-    alpha.eksctl.io/eksctl-version = "0.127.0"
-    alpha.eksctl.io/cluster-oidc-enabled = "true"
-    eksctl.cluster.k8s.io/v1alpha1/cluster-name = "kaiburrEks"
-    aws:cloudformation:stack-id = "arn:aws:cloudformation:us-east-1:164416580421:stack/eksctl-kaiburrEks-cluster/9c9ee500-ac7a-11ed-994f-0af03e412367"
-  }
-}
-
