64ddda3a76492a1b72171932b10290aee2468c77
Naman1997 <aroranaman17@gmail.com>
2023-06-08 23:34:41 +0530

    Updated main-8.tf

--- main-8.tf
+++ main-8.tf
@@ -1,10 +1,10 @@
-module "Subnet_18" {
+module "subnet_18" {
   cidr_block              = "172.31.64.0/20"
   map_public_ip_on_launch = true
   az                      = "use1-az5"
 }
 
-module "Subnet_19" {
+module "subnet_19" {
   cidr_block              = "192.168.160.0/19"
   map_public_ip_on_launch = false
   az                      = "use1-az2"
@@ -21,7 +21,7 @@ module "Subnet_19" {
   }
 }
 
-module "Subnet_20" {
+module "subnet_20" {
   cidr_block              = "192.168.128.0/19"
   map_public_ip_on_launch = false
   az                      = "use1-az1"
