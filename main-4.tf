f412f78d1a4f924c071bc98f4aa2470f7da864bd
Naman1997 <aroranaman17@gmail.com>
2023-06-09 21:12:02 +0530

    Updated main-4.tf

--- main-4.tf
+++ main-4.tf
@@ -1,21 +1,21 @@
 module "subnet_6" {
   cidr_block              = "172.31.80.0/20"
   map_public_ip_on_launch = true
-  vpc_id                  = module.vpc.vpc_1
+  vpc_id                  = module.vpc_1.id
   az                      = "use1-az2"
 }
 
 module "subnet_7" {
   cidr_block              = "172.31.32.0/20"
   map_public_ip_on_launch = true
-  vpc_id                  = module.vpc.vpc_1
+  vpc_id                  = module.vpc_1.id
   az                      = "use1-az6"
 }
 
 module "subnet_8" {
   cidr_block              = "192.168.160.0/19"
   map_public_ip_on_launch = false
-  vpc_id                  = module.vpc.vpc_4
+  vpc_id                  = module.vpc_4.id
   az                      = "use1-az2"
   tags                    = {
     alpha.eksctl.io/cluster-oidc-enabled = "true"
