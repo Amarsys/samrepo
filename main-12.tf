a8d28ec9933fbdbe1581d227662ac8f7fd98373d
Naman1997 <aroranaman17@gmail.com>
2023-06-12 13:36:50 -0700

    Updated main-12.tf

--- main-12.tf
+++ main-12.tf
@@ -2,14 +2,14 @@ module "internet_gateway_3" {
  source ="./modules/internet_gateway"
   vpc_id = "[vpc-0a18e3337585e7f2d]"
   tags   = {
-    alpha.eksctl.io/eksctl-version = "0.127.0"
-    aws:cloudformation:logical-id = "InternetGateway"
-    aws:cloudformation:stack-name = "eksctl-kaiburrEks-cluster"
-    Name = "eksctl-kaiburrEks-cluster/InternetGateway"
-    alpha.eksctl.io/cluster-oidc-enabled = "true"
-    aws:cloudformation:stack-id = "arn:aws:cloudformation:us-east-1:164416580421:stack/eksctl-kaiburrEks-cluster/9c9ee500-ac7a-11ed-994f-0af03e412367"
-    alpha.eksctl.io/cluster-name = "kaiburrEks"
-    eksctl.cluster.k8s.io/v1alpha1/cluster-name = "kaiburrEks"
+    "alpha.eksctl.io/eksctl-version" = "0.127.0"
+    "aws:cloudformation:logical-id" = "InternetGateway"
+    "aws:cloudformation:stack-name" = "eksctl-kaiburrEks-cluster"
+    "Name" = "eksctl-kaiburrEks-cluster/InternetGateway"
+    "alpha.eksctl.io/cluster-oidc-enabled" = "true"
+    "aws:cloudformation:stack-id" = "arn:aws:cloudformation:us-east-1:164416580421:stack/eksctl-kaiburrEks-cluster/9c9ee500-ac7a-11ed-994f-0af03e412367"
+    "alpha.eksctl.io/cluster-name" = "kaiburrEks"
+    "eksctl.cluster.k8s.io/v1alpha1/cluster-name" = "kaiburrEks"
   }
 }
 
