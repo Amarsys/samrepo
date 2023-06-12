6a4556aed26b88560f1a87f2f0b7c3b7a1c1f1a0
Naman1997 <aroranaman17@gmail.com>
2023-06-12 13:37:03 -0700

    Updated main-13.tf

--- main-13.tf
+++ main-13.tf
@@ -2,14 +2,14 @@ module "nat_route_table_2" {
  source ="./modules/nat_route_table"
   vpc_id         = module.vpc_3.id
   tags           = {
-    aws:cloudformation:stack-name = "eksctl-kaiburr-eks-cluster"
-    alpha.eksctl.io/cluster-name = "kaiburr-eks"
-    aws:cloudformation:stack-id = "arn:aws:cloudformation:us-east-1:164416580421:stack/eksctl-kaiburr-eks-cluster/b3e45610-a251-11ed-8758-1297053ce6d9"
-    alpha.eksctl.io/eksctl-version = "0.127.0"
-    Name = "eksctl-kaiburr-eks-cluster/PublicRouteTable"
-    aws:cloudformation:logical-id = "PublicRouteTable"
-    eksctl.cluster.k8s.io/v1alpha1/cluster-name = "kaiburr-eks"
-    alpha.eksctl.io/cluster-oidc-enabled = "true"
+    "aws:cloudformation:stack-name" = "eksctl-kaiburr-eks-cluster"
+    "alpha.eksctl.io/cluster-name" = "kaiburr-eks"
+    "aws:cloudformation:stack-id" = "arn:aws:cloudformation:us-east-1:164416580421:stack/eksctl-kaiburr-eks-cluster/b3e45610-a251-11ed-8758-1297053ce6d9"
+    "alpha.eksctl.io/eksctl-version" = "0.127.0"
+    "Name" = "eksctl-kaiburr-eks-cluster/PublicRouteTable"
+    "aws:cloudformation:logical-id" = "PublicRouteTable"
+    "eksctl.cluster.k8s.io/v1alpha1/cluster-name" = "kaiburr-eks"
+    "alpha.eksctl.io/cluster-oidc-enabled" = "true"
   }
 }
 
@@ -17,14 +17,14 @@ module "nat_route_table_3" {
  source ="./modules/nat_route_table"
   vpc_id         = module.vpc_4.id
   tags           = {
-    alpha.eksctl.io/cluster-name = "kaiburrEks"
-    alpha.eksctl.io/eksctl-version = "0.127.0"
-    aws:cloudformation:logical-id = "PublicRouteTable"
-    eksctl.cluster.k8s.io/v1alpha1/cluster-name = "kaiburrEks"
-    alpha.eksctl.io/cluster-oidc-enabled = "true"
-    aws:cloudformation:stack-name = "eksctl-kaiburrEks-cluster"
-    Name = "eksctl-kaiburrEks-cluster/PublicRouteTable"
-    aws:cloudformation:stack-id = "arn:aws:cloudformation:us-east-1:164416580421:stack/eksctl-kaiburrEks-cluster/9c9ee500-ac7a-11ed-994f-0af03e412367"
+    "alpha.eksctl.io/cluster-name" = "kaiburrEks"
+    "alpha.eksctl.io/eksctl-version" = "0.127.0"
+    "aws:cloudformation:logical-id" = "PublicRouteTable"
+    "eksctl.cluster.k8s.io/v1alpha1/cluster-name" = "kaiburrEks"
+    "alpha.eksctl.io/cluster-oidc-enabled" = "true"
+    "aws:cloudformation:stack-name" = "eksctl-kaiburrEks-cluster"
+    "Name" = "eksctl-kaiburrEks-cluster/PublicRouteTable"
+    "aws:cloudformation:stack-id" = "arn:aws:cloudformation:us-east-1:164416580421:stack/eksctl-kaiburrEks-cluster/9c9ee500-ac7a-11ed-994f-0af03e412367"
   }
 }
 
