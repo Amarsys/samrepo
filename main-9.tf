7b5c94333e1f71b196a51373f641d47b8526ca4e
Naman1997 <aroranaman17@gmail.com>
2023-06-12 13:36:11 -0700

    Updated main-9.tf

--- main-9.tf
+++ main-9.tf
@@ -5,15 +5,15 @@ module "subnet_21" {
   vpc_id                  = module.vpc_4.id
   az                      = "use1-az2"
   tags                    = {
-    alpha.eksctl.io/cluster-name = "kaiburrEks"
-    alpha.eksctl.io/eksctl-version = "0.127.0"
-    Name = "eksctl-kaiburrEks-cluster/SubnetPublicUSEAST1C"
-    alpha.eksctl.io/cluster-oidc-enabled = "true"
-    aws:cloudformation:stack-name = "eksctl-kaiburrEks-cluster"
-    kubernetes.io/role/elb = "1"
-    aws:cloudformation:logical-id = "SubnetPublicUSEAST1C"
-    aws:cloudformation:stack-id = "arn:aws:cloudformation:us-east-1:164416580421:stack/eksctl-kaiburrEks-cluster/9c9ee500-ac7a-11ed-994f-0af03e412367"
-    eksctl.cluster.k8s.io/v1alpha1/cluster-name = "kaiburrEks"
+    "alpha.eksctl.io/cluster-name" = "kaiburrEks"
+    "alpha.eksctl.io/eksctl-version" = "0.127.0"
+    "Name" = "eksctl-kaiburrEks-cluster/SubnetPublicUSEAST1C"
+    "alpha.eksctl.io/cluster-oidc-enabled" = "true"
+    "aws:cloudformation:stack-name" = "eksctl-kaiburrEks-cluster"
+    "kubernetes.io/role/elb" = "1"
+    "aws:cloudformation:logical-id" = "SubnetPublicUSEAST1C"
+    "aws:cloudformation:stack-id" = "arn:aws:cloudformation:us-east-1:164416580421:stack/eksctl-kaiburrEks-cluster/9c9ee500-ac7a-11ed-994f-0af03e412367"
+    "eksctl.cluster.k8s.io/v1alpha1/cluster-name" = "kaiburrEks"
   }
 }
 
@@ -24,15 +24,15 @@ module "subnet_22" {
   vpc_id                  = module.vpc_4.id
   az                      = "use1-az6"
   tags                    = {
-    kubernetes.io/role/internal-elb = "1"
-    alpha.eksctl.io/cluster-name = "kaiburrEks"
-    aws:cloudformation:logical-id = "SubnetPrivateUSEAST1A"
-    aws:cloudformation:stack-name = "eksctl-kaiburrEks-cluster"
-    alpha.eksctl.io/eksctl-version = "0.127.0"
-    alpha.eksctl.io/cluster-oidc-enabled = "true"
-    aws:cloudformation:stack-id = "arn:aws:cloudformation:us-east-1:164416580421:stack/eksctl-kaiburrEks-cluster/9c9ee500-ac7a-11ed-994f-0af03e412367"
-    Name = "eksctl-kaiburrEks-cluster/SubnetPrivateUSEAST1A"
-    eksctl.cluster.k8s.io/v1alpha1/cluster-name = "kaiburrEks"
+    "kubernetes.io/role/internal-elb" = "1"
+    "alpha.eksctl.io/cluster-name" = "kaiburrEks"
+    "aws:cloudformation:logical-id" = "SubnetPrivateUSEAST1A"
+    "aws:cloudformation:stack-name" = "eksctl-kaiburrEks-cluster"
+    "alpha.eksctl.io/eksctl-version" = "0.127.0"
+    "alpha.eksctl.io/cluster-oidc-enabled" = "true"
+    "aws:cloudformation:stack-id" = "arn:aws:cloudformation:us-east-1:164416580421:stack/eksctl-kaiburrEks-cluster/9c9ee500-ac7a-11ed-994f-0af03e412367"
+    "Name" = "eksctl-kaiburrEks-cluster/SubnetPrivateUSEAST1A"
+    "eksctl.cluster.k8s.io/v1alpha1/cluster-name" = "kaiburrEks"
   }
 }
 
