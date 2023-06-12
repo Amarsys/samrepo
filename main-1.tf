20291d265aee50c09fe421a7b30b85f9bf9139aa
Naman1997 <aroranaman17@gmail.com>
2023-06-12 07:42:03 -0700

    Updated main-1.tf

--- main-1.tf
+++ main-1.tf
@@ -1,32 +1,39 @@
-module "vpc_1" {
-  vpc_cidr = "172.31.0.0/16"
-}
-
-module "vpc_2" {
-  vpc_cidr = "192.168.0.0/16"
-  tags     = {
-    aws:cloudformation:logical-id = "VPC"
-    alpha.eksctl.io/cluster-name = "kaiburr-cluster"
-    aws:cloudformation:stack-name = "eksctl-kaiburr-cluster-cluster"
-    aws:cloudformation:stack-id = "arn:aws:cloudformation:us-east-1:164416580421:stack/eksctl-kaiburr-cluster-cluster/ac75a270-a70c-11ed-9155-12447c80f821"
-    alpha.eksctl.io/cluster-oidc-enabled = "true"
-    Name = "eksctl-kaiburr-cluster-cluster/VPC"
+module "internet_gateway_1" {
+  tags   = {
+    aws:cloudformation:logical-id = "InternetGateway"
     alpha.eksctl.io/eksctl-version = "0.127.0"
-    eksctl.cluster.k8s.io/v1alpha1/cluster-name = "kaiburr-cluster"
+    alpha.eksctl.io/cluster-oidc-enabled = "true"
+    aws:cloudformation:stack-id = "arn:aws:cloudformation:us-east-1:164416580421:stack/eksctl-kaiburr-eks-cluster/b3e45610-a251-11ed-8758-1297053ce6d9"
+    Name = "eksctl-kaiburr-eks-cluster/InternetGateway"
+    aws:cloudformation:stack-name = "eksctl-kaiburr-eks-cluster"
+    eksctl.cluster.k8s.io/v1alpha1/cluster-name = "kaiburr-eks"
+    alpha.eksctl.io/cluster-name = "kaiburr-eks"
   }
 }
 
-module "vpc_3" {
-  vpc_cidr = "192.168.0.0/16"
-  tags     = {
+module "internet_gateway_2" {
+  tags   = {
+    alpha.eksctl.io/cluster-name = "kaiburr-cluster"
+    eksctl.cluster.k8s.io/v1alpha1/cluster-name = "kaiburr-cluster"
     alpha.eksctl.io/cluster-oidc-enabled = "true"
-    aws:cloudformation:stack-id = "arn:aws:cloudformation:us-east-1:164416580421:stack/eksctl-kaiburr-eks-cluster/b3e45610-a251-11ed-8758-1297053ce6d9"
-    aws:cloudformation:logical-id = "VPC"
-    aws:cloudformation:stack-name = "eksctl-kaiburr-eks-cluster"
+    aws:cloudformation:logical-id = "InternetGateway"
+    aws:cloudformation:stack-id = "arn:aws:cloudformation:us-east-1:164416580421:stack/eksctl-kaiburr-cluster-cluster/ac75a270-a70c-11ed-9155-12447c80f821"
     alpha.eksctl.io/eksctl-version = "0.127.0"
-    Name = "eksctl-kaiburr-eks-cluster/VPC"
-    eksctl.cluster.k8s.io/v1alpha1/cluster-name = "kaiburr-eks"
-    alpha.eksctl.io/cluster-name = "kaiburr-eks"
+    Name = "eksctl-kaiburr-cluster-cluster/InternetGateway"
+    aws:cloudformation:stack-name = "eksctl-kaiburr-cluster-cluster"
+  }
+}
+
+module "internet_gateway_3" {
+  tags   = {
+    alpha.eksctl.io/eksctl-version = "0.127.0"
+    aws:cloudformation:logical-id = "InternetGateway"
+    aws:cloudformation:stack-name = "eksctl-kaiburrEks-cluster"
+    Name = "eksctl-kaiburrEks-cluster/InternetGateway"
+    alpha.eksctl.io/cluster-oidc-enabled = "true"
+    aws:cloudformation:stack-id = "arn:aws:cloudformation:us-east-1:164416580421:stack/eksctl-kaiburrEks-cluster/9c9ee500-ac7a-11ed-994f-0af03e412367"
+    alpha.eksctl.io/cluster-name = "kaiburrEks"
+    eksctl.cluster.k8s.io/v1alpha1/cluster-name = "kaiburrEks"
   }
 }
 
