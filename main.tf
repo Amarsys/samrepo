d5510c7ae5da9e4d6c55a0e553e2af5dde6a200d
Naman1997 <Naman1997@users.noreply.github.com>
2023-06-13 14:31:52 +0000

    Merged files into main.tf

--- main.tf
+++ main.tf
@@ -439,9 +439,8 @@ module "subnet_24" {
 }
 
 module "nat_gateway_1" {
-  source         = "./modules/nat_gateway"
-  nat_gateway_id = "nat-03124bae3c96ec462"
-  subnet_id      = module.subnet_24.id
+  source    = "./modules/nat_gateway"
+  subnet_id = module.subnet_24.id
   tags = {
     "aws:cloudformation:stack-name"               = "eksctl-kaiburr-cluster-cluster"
     "alpha.eksctl.io/cluster-name"                = "kaiburr-cluster"
@@ -455,9 +454,8 @@ module "nat_gateway_1" {
 }
 
 module "nat_gateway_2" {
-  source         = "./modules/nat_gateway"
-  nat_gateway_id = "nat-0f36b6713b247696a"
-  subnet_id      = module.subnet_14.id
+  source    = "./modules/nat_gateway"
+  subnet_id = module.subnet_14.id
   tags = {
     "aws:cloudformation:stack-name"               = "eksctl-kaiburrEks-cluster"
     "alpha.eksctl.io/cluster-name"                = "kaiburrEks"
@@ -471,9 +469,8 @@ module "nat_gateway_2" {
 }
 
 module "nat_gateway_3" {
-  source         = "./modules/nat_gateway"
-  nat_gateway_id = "nat-05cd4bbaadf4d0d59"
-  subnet_id      = module.subnet_3.id
+  source    = "./modules/nat_gateway"
+  subnet_id = module.subnet_3.id
   tags = {
     "aws:cloudformation:stack-name"               = "eksctl-kaiburr-eks-cluster"
     "alpha.eksctl.io/cluster-name"                = "kaiburr-eks"
@@ -540,51 +537,6 @@ module "internet_gateway_4" {
   vpc_id     = module.vpc_1.id
 }
 
-module "nat_route_table_1" {
-  source = "./modules/nat_route_table"
-  vpc_id = module.vpc_3.id
-}
-
-module "nat_route_table_2" {
-  source = "./modules/nat_route_table"
-  vpc_id = module.vpc_3.id
-  tags = {
-    "aws:cloudformation:stack-name"               = "eksctl-kaiburr-eks-cluster"
-    "alpha.eksctl.io/cluster-name"                = "kaiburr-eks"
-    "aws:cloudformation:stack-id"                 = "arn:aws:cloudformation:us-east-1:164416580421:stack/eksctl-kaiburr-eks-cluster/b3e45610-a251-11ed-8758-1297053ce6d9"
-    "alpha.eksctl.io/eksctl-version"              = "0.127.0"
-    "Name"                                        = "eksctl-kaiburr-eks-cluster/PublicRouteTable"
-    "aws:cloudformation:logical-id"               = "PublicRouteTable"
-    "eksctl.cluster.k8s.io/v1alpha1/cluster-name" = "kaiburr-eks"
-    "alpha.eksctl.io/cluster-oidc-enabled"        = "true"
-  }
-}
-
-module "nat_route_table_3" {
-  source = "./modules/nat_route_table"
-  vpc_id = module.vpc_4.id
-  tags = {
-    "alpha.eksctl.io/cluster-name"                = "kaiburrEks"
-    "alpha.eksctl.io/eksctl-version"              = "0.127.0"
-    "aws:cloudformation:logical-id"               = "PublicRouteTable"
-    "eksctl.cluster.k8s.io/v1alpha1/cluster-name" = "kaiburrEks"
-    "alpha.eksctl.io/cluster-oidc-enabled"        = "true"
-    "aws:cloudformation:stack-name"               = "eksctl-kaiburrEks-cluster"
-    "Name"                                        = "eksctl-kaiburrEks-cluster/PublicRouteTable"
-    "aws:cloudformation:stack-id"                 = "arn:aws:cloudformation:us-east-1:164416580421:stack/eksctl-kaiburrEks-cluster/9c9ee500-ac7a-11ed-994f-0af03e412367"
-  }
-}
-
-module "nat_route_table_4" {
-  source = "./modules/nat_route_table"
-  vpc_id = module.vpc_1.id
-}
-
-module "nat_route_table_5" {
-  source = "./modules/nat_route_table"
-  vpc_id = module.vpc_2.id
-}
-
 module "nat_route_table_6" {
   source         = "./modules/nat_route_table"
   nat_gateway_id = module.nat_gateway_3.id
@@ -598,21 +550,6 @@ module "nat_route_table_6" {
     "alpha.eksctl.io/cluster-oidc-enabled"        = "true"
     "aws:cloudformation:stack-id"                 = "arn:aws:cloudformation:us-east-1:164416580421:stack/eksctl-kaiburr-eks-cluster/b3e45610-a251-11ed-8758-1297053ce6d9"
     "alpha.eksctl.io/eksctl-version"              = "0.127.0"
-  }
-}
-
-module "nat_route_table_7" {
-  source = "./modules/nat_route_table"
-  vpc_id = module.vpc_2.id
-  tags = {
-    "alpha.eksctl.io/cluster-oidc-enabled"        = "true"
-    "alpha.eksctl.io/eksctl-version"              = "0.127.0"
-    "Name"                                        = "eksctl-kaiburr-cluster-cluster/PublicRouteTable"
-    "alpha.eksctl.io/cluster-name"                = "kaiburr-cluster"
-    "aws:cloudformation:stack-name"               = "eksctl-kaiburr-cluster-cluster"
-    "aws:cloudformation:stack-id"                 = "arn:aws:cloudformation:us-east-1:164416580421:stack/eksctl-kaiburr-cluster-cluster/ac75a270-a70c-11ed-9155-12447c80f821"
-    "aws:cloudformation:logical-id"               = "PublicRouteTable"
-    "eksctl.cluster.k8s.io/v1alpha1/cluster-name" = "kaiburr-cluster"
   }
 }
 
@@ -726,11 +663,6 @@ module "nat_route_table_14" {
     "alpha.eksctl.io/cluster-name"                = "kaiburrEks"
     "aws:cloudformation:stack-id"                 = "arn:aws:cloudformation:us-east-1:164416580421:stack/eksctl-kaiburrEks-cluster/9c9ee500-ac7a-11ed-994f-0af03e412367"
   }
-}
-
-module "nat_route_table_15" {
-  source = "./modules/nat_route_table"
-  vpc_id = module.vpc_4.id
 }
 
 module "nat_route_table_16" {
