8195a9d7e85711ab2389ebf3f76e280b8594cf5f
Naman1997 <aroranaman17@gmail.com>
2023-06-14 07:42:55 -0700

    Created main-19.tf

---
+++ main-19.tf
@@ -0,0 +1,24 @@
+module "route_table_association_13" {
+ source ="./modules/route_table_association"
+  subnet_ids     = {
+  subnet_1 = module.subnet_2.subnet_id
+  }
+  route_table_id = module.nat_route_table_13.route_table_id
+}
+
+module "route_table_association_14" {
+ source ="./modules/route_table_association"
+  subnet_ids     = {
+  subnet_1 = module.subnet_8.subnet_id
+  }
+  route_table_id = module.nat_route_table_14.route_table_id
+}
+
+module "route_table_association_16" {
+ source ="./modules/route_table_association"
+  subnet_ids     = {
+  subnet_1 = module.subnet_20.subnet_id
+  }
+  route_table_id = module.nat_route_table_16.route_table_id
+}
+
