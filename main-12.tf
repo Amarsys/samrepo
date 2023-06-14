f45008524b95683688d4289d70911e129810112b
Naman1997 <aroranaman17@gmail.com>
2023-06-14 12:10:25 -0700

    Created main-12.tf

---
+++ main-12.tf
@@ -0,0 +1,18 @@
+module "internet_gateway_3" {
+ source ="./modules/internet_gateway"
+  tags   = {
+    "alpha.eksctl.io/eksctl-version" = "0.127.0"
+    "aws:cloudformation:logical-id" = "InternetGateway"
+    "aws:cloudformation:stack-name" = "eksctl-kaiburrEks-cluster"
+    "Name" = "eksctl-kaiburrEks-cluster/InternetGateway"
+    "alpha.eksctl.io/cluster-oidc-enabled" = "true"
+    "aws:cloudformation:stack-id" = "arn:aws:cloudformation:us-east-1:164416580421:stack/eksctl-kaiburrEks-cluster/9c9ee500-ac7a-11ed-994f-0af03e412367"
+    "alpha.eksctl.io/cluster-name" = "kaiburrEks"
+    "eksctl.cluster.k8s.io/v1alpha1/cluster-name" = "kaiburrEks"
+  }
+}
+
+module "internet_gateway_4" {
+ source ="./modules/internet_gateway"
+}
+
