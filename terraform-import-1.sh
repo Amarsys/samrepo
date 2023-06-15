756b849c9d619414aff97d827314b7d98f8f2413
Naman Arora <aroranaman17@outlook.com>
2023-06-16 02:29:51 +0530

    Delete terraform-import-1.sh

--- terraform-import-1.sh
+++
@@ -1,6 +0,0 @@
-terraform import module.vpc_1.aws_vpc.vpcvpc-ca1dbeb7
-
-terraform import module.vpc_2.aws_vpc.vpcvpc-03811f75a97cffa86
-
-terraform import module.vpc_3.aws_vpc.vpcvpc-07452964709d9cc87
-
