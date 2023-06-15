0cabe6f220f732ecafceaf6f3888b6b461b5048e
Naman1997 <aroranaman17@gmail.com>
2023-06-15 14:09:02 -0700

    Created terraform-import-10.sh

---
+++ terraform-import-10.sh
@@ -0,0 +1,3 @@
+terraform import module.subnet_24.aws_subnet.subnet subnet-06922b7e140c164fb
+terraform import module.nat_gateway_1.aws_nat_gateway.nat nat-03124bae3c96ec462
+terraform import module.nat_gateway_2.aws_nat_gateway.nat nat-0f36b6713b247696a
