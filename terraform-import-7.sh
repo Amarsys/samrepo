0a59096f31be1606fd5e9bb8629c5449478de771
Naman1997 <aroranaman17@gmail.com>
2023-06-15 15:10:49 -0700

    Created terraform-import-7.sh

---
+++ terraform-import-7.sh
@@ -0,0 +1,3 @@
+terraform import module.subnet_15.aws_subnet.subnet subnet-08bd3ebf13e6078b3
+terraform import module.subnet_16.aws_subnet.subnet subnet-0afc6a6c
+terraform import module.nat_gateway_1.aws_nat_gateway.nat nat-03124bae3c96ec462
