e5d89067194c04ccfa6c0f0c180c69b5e526d794
Naman1997 <Naman1997@users.noreply.github.com>
2023-06-15 21:16:41 +0000

    Merged files into main.tf

---
+++ terraform-import.sh
@@ -0,0 +1,57 @@
+terraform import module.vpc_1.aws_vpc.vpc vpc-ca1dbeb7
+terraform import module.vpc_2.aws_vpc.vpc vpc-03811f75a97cffa86
+terraform import module.vpc_3.aws_vpc.vpc vpc-07452964709d9cc87
+terraform import module.vpc_4.aws_vpc.vpc vpc-0a18e3337585e7f2d
+terraform import module.subnet_1.aws_subnet.subnet subnet-07e509d5bbeb86ee3
+terraform import module.subnet_2.aws_subnet.subnet subnet-01c47000df369b6fb
+terraform import module.subnet_3.aws_subnet.subnet subnet-0e643c573c7daeac7
+terraform import module.subnet_4.aws_subnet.subnet subnet-0625ded7cd9ba3e58
+terraform import module.subnet_5.aws_subnet.subnet subnet-09e0aa166d38b49a0
+terraform import module.subnet_6.aws_subnet.subnet subnet-b11e8390
+terraform import module.subnet_7.aws_subnet.subnet subnet-7a57c425
+terraform import module.subnet_8.aws_subnet.subnet subnet-03385fcee5457b536
+terraform import module.subnet_9.aws_subnet.subnet subnet-0789d52c3f3045e17
+terraform import module.subnet_10.aws_subnet.subnet subnet-04300872857f03c3a
+terraform import module.subnet_11.aws_subnet.subnet subnet-911731dc
+terraform import module.subnet_12.aws_subnet.subnet subnet-0924c7ab5eb22c900
+terraform import module.subnet_13.aws_subnet.subnet subnet-0b30741613af82435
+terraform import module.subnet_14.aws_subnet.subnet subnet-0b7f54fd77c59e881
+terraform import module.subnet_15.aws_subnet.subnet subnet-08bd3ebf13e6078b3
+terraform import module.subnet_16.aws_subnet.subnet subnet-0afc6a6c
+terraform import module.subnet_17.aws_subnet.subnet subnet-0777c89717e03b99a
+terraform import module.subnet_18.aws_subnet.subnet subnet-137b331d
+terraform import module.subnet_19.aws_subnet.subnet subnet-0155ddbcf6d005cda
+terraform import module.subnet_20.aws_subnet.subnet subnet-05dd600368666de8e
+terraform import module.subnet_21.aws_subnet.subnet subnet-02be2e5a15db5f53b
+terraform import module.subnet_22.aws_subnet.subnet subnet-08e5d4685fea3bd4b
+terraform import module.subnet_23.aws_subnet.subnet subnet-db518bea
+terraform import module.subnet_24.aws_subnet.subnet subnet-06922b7e140c164fb
+terraform import module.nat_gateway_1.aws_nat_gateway.nat nat-03124bae3c96ec462
+terraform import module.nat_gateway_2.aws_nat_gateway.nat nat-0f36b6713b247696a
+terraform import module.nat_gateway_3.aws_nat_gateway.nat nat-05cd4bbaadf4d0d59
+terraform import module.internet_gateway_1.aws_internet_gateway.igw igw-0215bdec00ad5feab
+terraform import module.internet_gateway_2.aws_internet_gateway.igw igw-028572ac3629d2854
+terraform import module.internet_gateway_3.aws_internet_gateway.igw igw-0e65dd3b705aa4069
+terraform import module.internet_gateway_4.aws_internet_gateway.igw igw-17a5b36c
+terraform import module.nat_route_table_6.aws_route_table.route_table rtb-0f402c5143c95eb75
+terraform import module.nat_route_table_8.aws_route_table.route_table rtb-024f129dbcf626421
+terraform import module.nat_route_table_9.aws_route_table.route_table rtb-0dbea13d546f4ede6
+terraform import module.nat_route_table_10.aws_route_table.route_table rtb-05cc63256aba18e9b
+terraform import module.nat_route_table_11.aws_route_table.route_table rtb-0b7b8113a22090948
+terraform import module.nat_route_table_12.aws_route_table.route_table rtb-0601a2d557c2adf8c
+terraform import module.nat_route_table_13.aws_route_table.route_table rtb-01403800ddff03862
+terraform import module.nat_route_table_14.aws_route_table.route_table rtb-016cb5663d271d23c
+terraform import module.nat_route_table_16.aws_route_table.route_table rtb-0d95fc94900a4a175
+terraform import module.igw_route_table_2.aws_route_table.route_table rtb-0b208019ff38cd799
+terraform import module.igw_route_table_3.aws_route_table.route_table rtb-042c633b40617bee3
+terraform import module.igw_route_table_4.aws_route_table.route_table rtb-f6567f88
+terraform import module.igw_route_table_7.aws_route_table.route_table rtb-0909ca83f4c3f313a
+associations.subnetId/routeTableId
+associations.subnetId/routeTableId
+associations.subnetId/routeTableId
+associations.subnetId/routeTableId
+associations.subnetId/routeTableId
+associations.subnetId/routeTableId
+associations.subnetId/routeTableId
+associations.subnetId/routeTableId
+associations.subnetId/routeTableId
