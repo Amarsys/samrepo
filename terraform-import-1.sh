938e1afd3ecfb5f1d4ffe4b426f6ffc6fdffc2ea
Naman1997 <aroranaman17@gmail.com>
2023-06-15 14:04:41 -0700

    Created terraform-import-1.sh

---
+++ terraform-import-1.sh
@@ -0,0 +1,3 @@
+terraform import module.vpc_1.aws_vpc.vpc vpc-ca1dbeb7
+terraform import module.vpc_2.aws_vpc.vpc vpc-03811f75a97cffa86
+terraform import module.vpc_3.aws_vpc.vpc vpc-07452964709d9cc87
