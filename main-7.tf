06a9d3546b44c87f33aeeba9ddcfe4f9b0bf84fa
Naman1997 <aroranaman17@gmail.com>
2023-06-15 15:23:27 -0700

    Updated main-7.tf

--- main-7.tf
+++ main-7.tf
@@ -25,17 +25,22 @@ module "subnet_16" {
   az                      = "use1-az1"
 }
 
-module "nat_gateway_1" {
- source ="./modules/nat_gateway"
-  tags      = {
-    "aws:cloudformation:stack-name" = "eksctl-kaiburr-cluster-cluster"
-    "alpha.eksctl.io/cluster-name" = "kaiburr-cluster"
-    "aws:cloudformation:stack-id" = "arn:aws:cloudformation:us-east-1:164416580421:stack/eksctl-kaiburr-cluster-cluster/ac75a270-a70c-11ed-9155-12447c80f821"
-    "eksctl.cluster.k8s.io/v1alpha1/cluster-name" = "kaiburr-cluster"
+module "subnet_17" {
+ source ="./modules/subnet"
+  cidr_block              = "192.168.96.0/19"
+  map_public_ip_on_launch = false
+  vpc_id                  = module.vpc_3.id
+  az                      = "use1-az6"
+  tags                    = {
+    "aws:cloudformation:logical-id" = "SubnetPrivateUSEAST1A"
     "alpha.eksctl.io/cluster-oidc-enabled" = "true"
-    "aws:cloudformation:logical-id" = "NATGateway"
+    "aws:cloudformation:stack-name" = "eksctl-kaiburr-eks-cluster"
+    "kubernetes.io/role/internal-elb" = "1"
+    "alpha.eksctl.io/cluster-name" = "kaiburr-eks"
+    "eksctl.cluster.k8s.io/v1alpha1/cluster-name" = "kaiburr-eks"
     "alpha.eksctl.io/eksctl-version" = "0.127.0"
-    "Name" = "eksctl-kaiburr-cluster-cluster/NATGateway"
+    "aws:cloudformation:stack-id" = "arn:aws:cloudformation:us-east-1:164416580421:stack/eksctl-kaiburr-eks-cluster/b3e45610-a251-11ed-8758-1297053ce6d9"
+    "Name" = "eksctl-kaiburr-eks-cluster/SubnetPrivateUSEAST1A"
   }
 }
 
