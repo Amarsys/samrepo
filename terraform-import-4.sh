f627f6075b3d0eb2490eb24682321e591c8c0b48
Naman Arora <aroranaman17@outlook.com>
2023-06-16 02:30:34 +0530

    Delete terraform-import-4.sh

--- terraform-import-4.sh
+++
@@ -1,6 +0,0 @@
-terraform import module.subnet_6.aws_subnet.subnetsubnet-b11e8390
-
-terraform import module.subnet_7.aws_subnet.subnetsubnet-7a57c425
-
-terraform import module.subnet_8.aws_subnet.subnetsubnet-03385fcee5457b536
-
