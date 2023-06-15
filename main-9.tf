11fea2c509449c14a56b7dee17b0e9efdf4599dc
Naman1997 <aroranaman17@gmail.com>
2023-06-15 14:32:50 -0700

    Created main-9.tf

---
+++ main-9.tf
@@ -0,0 +1,46 @@
+module "subnet_21" {
+ source ="./modules/subnet"
+  cidr_block              = "192.168.64.0/19"
+  map_public_ip_on_launch = true
+  vpc_id                  = module.vpc_4.id
+  az                      = "use1-az2"
+  tags                    = {
+    "alpha.eksctl.io/cluster-name" = "kaiburrEks"
+    "alpha.eksctl.io/eksctl-version" = "0.127.0"
+    "Name" = "eksctl-kaiburrEks-cluster/SubnetPublicUSEAST1C"
+    "alpha.eksctl.io/cluster-oidc-enabled" = "true"
+    "aws:cloudformation:stack-name" = "eksctl-kaiburrEks-cluster"
+    "kubernetes.io/role/elb" = "1"
+    "aws:cloudformation:logical-id" = "SubnetPublicUSEAST1C"
+    "aws:cloudformation:stack-id" = "arn:aws:cloudformation:us-east-1:164416580421:stack/eksctl-kaiburrEks-cluster/9c9ee500-ac7a-11ed-994f-0af03e412367"
+    "eksctl.cluster.k8s.io/v1alpha1/cluster-name" = "kaiburrEks"
+  }
+}
+
+module "subnet_22" {
+ source ="./modules/subnet"
+  cidr_block              = "192.168.96.0/19"
+  map_public_ip_on_launch = false
+  vpc_id                  = module.vpc_4.id
+  az                      = "use1-az6"
+  tags                    = {
+    "kubernetes.io/role/internal-elb" = "1"
+    "alpha.eksctl.io/cluster-name" = "kaiburrEks"
+    "aws:cloudformation:logical-id" = "SubnetPrivateUSEAST1A"
+    "aws:cloudformation:stack-name" = "eksctl-kaiburrEks-cluster"
+    "alpha.eksctl.io/eksctl-version" = "0.127.0"
+    "alpha.eksctl.io/cluster-oidc-enabled" = "true"
+    "aws:cloudformation:stack-id" = "arn:aws:cloudformation:us-east-1:164416580421:stack/eksctl-kaiburrEks-cluster/9c9ee500-ac7a-11ed-994f-0af03e412367"
+    "Name" = "eksctl-kaiburrEks-cluster/SubnetPrivateUSEAST1A"
+    "eksctl.cluster.k8s.io/v1alpha1/cluster-name" = "kaiburrEks"
+  }
+}
+
+module "subnet_23" {
+ source ="./modules/subnet"
+  cidr_block              = "172.31.48.0/20"
+  map_public_ip_on_launch = true
+  vpc_id                  = module.vpc_1.id
+  az                      = "use1-az3"
+}
+
