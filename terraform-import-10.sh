7ca051e78ded828691ff60eab494d597cdcebf1c
Naman1997 <aroranaman17@gmail.com>
2023-06-16 07:10:35 -0700

    Created terraform-import-10.sh

---
+++ terraform-import-10.sh
@@ -0,0 +1,3 @@
+terraform import module.subnet_23.aws_subnet.subnet subnet-0afc6a6c
+terraform import module.subnet_24.aws_subnet.subnet subnet-0155ddbcf6d005cda
+terraform import module.subnet_25.aws_subnet.subnet subnet-0b23dc29d0eb227ce
