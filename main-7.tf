deb344ccc5fde3e4d58fa4c15776aafca28072e5
Naman1997 <aroranaman17@gmail.com>
2023-06-16 22:22:38 +0530

    Created main-7.tf

---
+++ main-7.tf
@@ -0,0 +1,48 @@
+module "subnet_14" {
+ source ="./modules/subnet"
+  cidr_block              = "192.168.0.0/19"
+  map_public_ip_on_launch = true
+  vpc_id                  = module.vpc_2.id
+  az                      = "us-east-1a"
+  tags                    = {
+    "Name" = "eksctl-kaiburr-cluster-cluster/SubnetPublicUSEAST1A"
+    "alpha.eksctl.io/eksctl-version" = "0.127.0"
+    "alpha.eksctl.io/cluster-oidc-enabled" = "true"
+    "alpha.eksctl.io/cluster-name" = "kaiburr-cluster"
+    "kubernetes.io/role/elb" = "1"
+    "eksctl.cluster.k8s.io/v1alpha1/cluster-name" = "kaiburr-cluster"
+  }
+}
+
+module "subnet_15" {
+ source ="./modules/subnet"
+  cidr_block              = "192.168.96.0/19"
+  map_public_ip_on_launch = false
+  vpc_id                  = module.vpc_2.id
+  az                      = "us-east-1a"
+  tags                    = {
+    "alpha.eksctl.io/cluster-oidc-enabled" = "true"
+    "alpha.eksctl.io/eksctl-version" = "0.127.0"
+    "eksctl.cluster.k8s.io/v1alpha1/cluster-name" = "kaiburr-cluster"
+    "Name" = "eksctl-kaiburr-cluster-cluster/SubnetPrivateUSEAST1A"
+    "alpha.eksctl.io/cluster-name" = "kaiburr-cluster"
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+}
+
+module "subnet_16" {
+ source ="./modules/subnet"
+  cidr_block              = "192.168.32.0/19"
+  map_public_ip_on_launch = true
+  vpc_id                  = module.vpc_5.id
+  az                      = "us-east-1b"
+  tags                    = {
+    "kubernetes.io/role/elb" = "1"
+    "Name" = "eksctl-kaiburrEks-cluster/SubnetPublicUSEAST1B"
+    "alpha.eksctl.io/cluster-oidc-enabled" = "true"
+    "alpha.eksctl.io/eksctl-version" = "0.127.0"
+    "alpha.eksctl.io/cluster-name" = "kaiburrEks"
+    "eksctl.cluster.k8s.io/v1alpha1/cluster-name" = "kaiburrEks"
+  }
+}
+
