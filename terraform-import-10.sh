7b0b937cd52e6c75be06e41598a6277778ac9c59
Naman1997 <aroranaman17@gmail.com>
2023-06-16 07:02:20 +0530

    Update test

--- terraform-import-10.sh
+++
@@ -1,5 +0,0 @@
-terraform import module.subnet_24.aws_subnet.subnet subnet-06922b7e140c164fb
-terraform import module.nat_gateway_1.aws_eip.nat_eip 3.214.221.186
-terraform import module.nat_gateway_1.aws_nat_gateway.nat nat-03124bae3c96ec462
-terraform import module.nat_gateway_2.aws_eip.nat_eip 23.21.65.107
-terraform import module.nat_gateway_2.aws_nat_gateway.nat nat-0f36b6713b247696a
