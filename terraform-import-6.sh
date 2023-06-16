db89b23d85522fda31575fc254803028ad8ab0ae
Naman1997 <aroranaman17@gmail.com>
2023-06-16 21:26:23 +0530

    Created terraform-import-6.sh

---
+++ terraform-import-6.sh
@@ -0,0 +1,3 @@
+terraform import module.subnet_11.aws_subnet.subnet subnet-02be2e5a15db5f53b
+terraform import module.subnet_12.aws_subnet.subnet subnet-08e5d4685fea3bd4b
+terraform import module.subnet_13.aws_subnet.subnet subnet-db518bea
