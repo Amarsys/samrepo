7f36ba35834b11646c2c015ea4faf7fd6d007c66
Naman1997 <aroranaman17@gmail.com>
2023-06-16 22:23:42 +0530

    Created terraform-import-9.sh

---
+++ terraform-import-9.sh
@@ -0,0 +1,3 @@
+terraform import module.subnet_20.aws_subnet.subnet subnet-0789d52c3f3045e17
+terraform import module.subnet_21.aws_subnet.subnet subnet-911731dc
+terraform import module.subnet_22.aws_subnet.subnet subnet-0b30741613af82435
