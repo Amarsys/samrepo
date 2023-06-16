f2fbe136a9b611ffba609214717dda8ad2e8dc5a
Naman1997 <aroranaman17@gmail.com>
2023-06-16 22:09:36 +0530

    Created main-17.tf

---
+++ main-17.tf
@@ -0,0 +1,23 @@
+module "igw_route_table_4" {
+ source ="./modules/igw_route_table"
+  vpc_id     = module.vpc_1.id
+  gateway_id = module.internet_gateway_4.igw_id
+}
+
+module "igw_route_table_7" {
+ source ="./modules/igw_route_table"
+  vpc_id     = module.vpc_2.id
+  gateway_id = module.internet_gateway_2.igw_id
+  tags       = {
+    "alpha.eksctl.io/cluster-oidc-enabled" = "true"
+    "alpha.eksctl.io/eksctl-version" = "0.127.0"
+    "Name" = "eksctl-kaiburr-cluster-cluster/PublicRouteTable"
+    "alpha.eksctl.io/cluster-name" = "kaiburr-cluster"
+    "eksctl.cluster.k8s.io/v1alpha1/cluster-name" = "kaiburr-cluster"
+  }
+}
+
+module "route_table_association_1" {
+ source ="./modules/route_table_association"
+}
+
