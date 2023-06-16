1df2a463517dba75929ddea8463908720018a7e5
Naman1997 <aroranaman17@gmail.com>
2023-06-16 05:42:27 -0700

    Created terraform-import-11.sh

---
+++ terraform-import-11.sh
@@ -0,0 +1,4 @@
+terraform import module.nat_gateway_3.aws_eip.nat_eip eipalloc-0494c690c318de51a
+terraform import module.nat_gateway_3.aws_nat_gateway.nat nat-05cd4bbaadf4d0d59
+terraform import module.internet_gateway_1.aws_internet_gateway.igw igw-0215bdec00ad5feab
+terraform import module.internet_gateway_2.aws_internet_gateway.igw igw-028572ac3629d2854
