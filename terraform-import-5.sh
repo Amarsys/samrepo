8817c5daed4d91da42a53c9bb93712a175dad4b7
Naman1997 <Naman1997@users.noreply.github.com>
2023-06-16 01:48:44 +0000

    Merged files into main.tf

--- terraform-import-5.sh
+++
@@ -1,3 +0,0 @@
-terraform import module.subnet_9.aws_subnet.subnet subnet-0789d52c3f3045e17
-terraform import module.subnet_10.aws_subnet.subnet subnet-04300872857f03c3a
-terraform import module.subnet_11.aws_subnet.subnet subnet-911731dc
--- terraform-import-8.sh
+++
@@ -1,3 +0,0 @@
-terraform import module.subnet_18.aws_subnet.subnet subnet-137b331d
-terraform import module.subnet_19.aws_subnet.subnet subnet-0155ddbcf6d005cda
-terraform import module.subnet_20.aws_subnet.subnet subnet-05dd600368666de8e
