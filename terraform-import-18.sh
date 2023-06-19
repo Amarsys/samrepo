68d5f9f72a61f790db11419c7bd1a7d86c633e08
Naman1997 <aroranaman17@gmail.com>
2023-06-19 22:09:34 +0530

    Created terraform-import-18.sh

---
+++ terraform-import-18.sh
@@ -0,0 +1,3 @@
+terraform import module.nat_route_table_16.aws_route_table.route_table rtb-01403800ddff03862
+terraform import module.nat_route_table_17.aws_route_table.route_table rtb-016cb5663d271d23c
+terraform import module.nat_route_table_20.aws_route_table.route_table rtb-0d95fc94900a4a175
