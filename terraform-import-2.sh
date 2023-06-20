6776958d3e1d4efaa0839819c5177a3af12d6033
Naman1997 <aroranaman17@gmail.com>
2023-06-21 00:42:06 +0530

    Created terraform-import-2.sh

---
+++ terraform-import-2.sh
@@ -0,0 +1,3 @@
+terraform import module.vpc_vpc-01824193402915c0a.aws_vpc.vpc vpc-01824193402915c0a
+terraform import module.vpc_vpc-0a18e3337585e7f2d.aws_vpc.vpc vpc-0a18e3337585e7f2d
+terraform import module.subnet_subnet-07e509d5bbeb86ee3.aws_subnet.subnet subnet-07e509d5bbeb86ee3
