523e357f127db01ddaa2e26d3874359c914e9f81
Naman1997 <aroranaman17@gmail.com>
2023-06-21 00:52:48 +0530

    Created terraform-import-22.sh

---
+++ terraform-import-22.sh
@@ -0,0 +1,3 @@
+terraform import module.route_table_association_rtb-024f129dbcf626421.aws_route_table_association.route_table_association[\"subnet_1\"] subnet-09e0aa166d38b49a0/rtb-024f129dbcf626421
+terraform import module.route_table_association_rtb-0dbea13d546f4ede6.aws_route_table_association.route_table_association[\"subnet_1\"] subnet-07e509d5bbeb86ee3/rtb-0dbea13d546f4ede6
+terraform import module.route_table_association_rtb-05cc63256aba18e9b.aws_route_table_association.route_table_association[\"subnet_1\"] subnet-08e5d4685fea3bd4b/rtb-05cc63256aba18e9b
