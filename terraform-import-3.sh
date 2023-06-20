23842ffb86cdd21f1b491e3205d3e0de30d5bf1d
Naman1997 <aroranaman17@gmail.com>
2023-06-20 22:23:53 +0530

    Created terraform-import-3.sh

---
+++ terraform-import-3.sh
@@ -0,0 +1,3 @@
+terraform import module.subnet_subnet-0f6c1a37a7c2ae9ba.aws_subnet.subnet subnet-0f6c1a37a7c2ae9ba
+terraform import module.subnet_subnet-092a04285bc3f6c83.aws_subnet.subnet subnet-092a04285bc3f6c83
+terraform import module.subnet_subnet-0e643c573c7daeac7.aws_subnet.subnet subnet-0e643c573c7daeac7
