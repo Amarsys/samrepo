f053fbf54fcfcdb43482645525ac1a3750e8ffea
Naman1997 <aroranaman17@gmail.com>
2023-06-20 22:32:14 +0530

    Created terraform-import-19.sh

---
+++ terraform-import-19.sh
@@ -0,0 +1,3 @@
+terraform import module.igw_route_table_rtb-0b208019ff38cd799.aws_route_table.route_table rtb-0b208019ff38cd799
+terraform import module.igw_route_table_rtb-042c633b40617bee3.aws_route_table.route_table rtb-042c633b40617bee3
+terraform import module.igw_route_table_rtb-f6567f88.aws_route_table.route_table rtb-f6567f88
