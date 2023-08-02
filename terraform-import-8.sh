9483c3ac19cd0d1e012751fa4f0363c4973212a3
rohithgadu <gadurohith@gmail.com>
2023-08-02 17:56:02 +0530

    Created terraform-import-8.sh

---
+++ terraform-import-8.sh
@@ -0,0 +1,3 @@
+terraform import module.subnet_subnet-08826711deab4dd2b.aws_subnet.subnet subnet-08826711deab4dd2b
+terraform import module.subnet_subnet-03039b1456ea05ec2.aws_subnet.subnet subnet-03039b1456ea05ec2
+terraform import module.subnet_subnet-068258798beb2d770.aws_subnet.subnet subnet-068258798beb2d770
