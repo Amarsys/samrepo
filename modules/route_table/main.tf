f0ce67e961ea54d42d6e202c38920d935528b9b5
vishwajit81 <vishwajit81@gmail.com>
2023-08-02 20:26:32 +0530

    Update main.tf

--- modules/route_table/main.tf
+++ modules/route_table/main.tf
@@ -3,19 +3,19 @@ resource "aws_route_table" "route_table" {
     dynamic "route" {
     for_each = var.routes
     content {
-      carrier_gateway_id = lookup(route.value, "carrier_gateway_id", null)
-      cidr_block = lookup(route.value, "cidr_block", null)
+      carrier_gateway_id = lookup(route.value, "CarrierGatewayId", null)
+      cidr_block = lookup(route.value, "destinationCidrBlock", null)
       core_network_arn = lookup(route.value, "core_network_arn", null)
-      destination_prefix_list_id = lookup(route.value, "destination_prefix_list_id", null)
-      egress_only_gateway_id = lookup(route.value, "egress_only_gateway_id", null)
-      gateway_id = lookup(route.value, "gateway_id", null)
-      ipv6_cidr_block = lookup(route.value, "ipv6_cidr_block", null)
-      local_gateway_id = lookup(route.value, "local_gateway_id", null)
-      nat_gateway_id = lookup(route.value, "nat_gateway_id", null)
-      network_interface_id = lookup(route.value, "network_interface_id", null)
-      transit_gateway_id = lookup(route.value, "transit_gateway_id", null)
-      vpc_endpoint_id = lookup(route.value, "vpc_endpoint_id", null)
-      vpc_peering_connection_id = lookup(route.value, "vpc_peering_connection_id", null)
+      destination_prefix_list_id = lookup(route.value, "destinationPrefixListId", null)
+      egress_only_gateway_id = lookup(route.value, "egressOnlyInternetGatewayId", null)
+      gateway_id = lookup(route.value, "gatewayId", null)
+      ipv6_cidr_block = lookup(route.value, "destinationIpv6CidrBlock", null)
+      local_gateway_id = lookup(route.value, "localGatewayId", null)
+      nat_gateway_id = lookup(route.value, "natGatewayId", null)
+      network_interface_id = lookup(route.value, "networkInterfaceId", null)
+      transit_gateway_id = lookup(route.value, "transitGatewayId", null)
+      vpc_endpoint_id = lookup(route.value, "vpcEndpointId", null)
+      vpc_peering_connection_id = lookup(route.value, "vpcPeeringConnectionId", null)
     }
   }
   tags = var.tags
