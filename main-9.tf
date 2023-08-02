ac21ddcef24e5e3b546777c180b35c27bbecff29
rohithgadu <gadurohith@gmail.com>
2023-08-02 19:22:42 +0530

    Created main-9.tf

---
+++ main-9.tf
@@ -0,0 +1,19 @@
+module "route_table_rtb-0ea77514b722cf99c" {
+ source ="./modules/route_table"
+  routes = [
+  {
+  },
+  {
+    "destinationCidrBlock" = "0.0.0.0/0"
+    "gatewayId" = "igw-04d860a3cfd672f23"
+  }
+  ]
+  vpc_id = module.vpc_vpc-040873ce6d24dfd97.id
+  tags   = {
+    "Region" = "us-east-1"
+    "Environment" = "Dev"
+    "Project" = "CGP"
+    "Name" = "dev_cgp_public_rt"
+  }
+}
+
