dae420e24a7b903780123611b767e80fa7434f47
rohithgadu <gadurohith@gmail.com>
2023-08-02 17:56:46 +0530

    Created terraform-import-11.sh

---
+++ terraform-import-11.sh
@@ -0,0 +1,3 @@
+terraform import module.subnet_subnet-04aa0e1275bceb3c2.aws_subnet.subnet subnet-04aa0e1275bceb3c2
+terraform import module.subnet_subnet-0790bd49408a2a74a.aws_subnet.subnet subnet-0790bd49408a2a74a
+terraform import module.subnet_subnet-0a2b5dda4cdee923f.aws_subnet.subnet subnet-0a2b5dda4cdee923f
