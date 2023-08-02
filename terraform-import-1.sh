ba977be952037d240a096b47405d879d6194983a
rohithgadu <gadurohith@gmail.com>
2023-08-02 17:17:42 +0530

    Created terraform-import-1.sh

---
+++ terraform-import-1.sh
@@ -0,0 +1,3 @@
+terraform import module.vpc_vpc-ca1dbeb7.aws_vpc.vpc vpc-ca1dbeb7
+terraform import module.vpc_vpc-01824193402915c0a.aws_vpc.vpc vpc-01824193402915c0a
+terraform import module.vpc_vpc-03707a836eae63075.aws_vpc.vpc vpc-03707a836eae63075
